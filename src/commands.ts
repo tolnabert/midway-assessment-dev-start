68421a3f67d1d11b960aea83ed476c5d355a4a3a
tolnabert <tolnabert@gmail.com>
2024-11-07 11:47:27 +0100

    add validation to list command, to not accept any arguments

--- src/commands.ts
+++ src/commands.ts
@@ -1,9 +1,14 @@
-import { Recipe, RecipeType } from "./recipe";
-import { Store } from "./stores/store.type";
+import { Recipe, RecipeType } from './recipe';
+import { Store } from './stores/store.type';
 
 export async function list(store: Store<RecipeType[]>, args: string[]) {
   const recipe = new Recipe(store);
   const recipes = await recipe.readAll();
+
+  if(args.length > 0) {
+    throw new Error ("Wrong command: list should not have any arguments")
+  }
+
   const formatted = recipes
     .map((recipe) => `- [${recipe.id}] ${recipe.name}`)
     .join('\n');
