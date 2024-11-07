409f70d83e2c973ea61b3cdd947ecf259ef6e6b3
tolnabert <tolnabert@gmail.com>
2024-11-07 12:11:39 +0100

    finish midway-assesment 3 task

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,20 @@
 import { AppError } from './app.error';
-import { list } from './commands';
+import { detailsByRecipeId, list } from './commands';
 import { Store } from './stores/store.type';
 import { RecipeType } from './recipe';
 
-type Command = (store: Store<RecipeType[]>, args: string[]) => Promise<void>;
+type Command = (store: Store<RecipeType[]>, args: string[]) => Promise<void>
 
-export async function createApp(store: Store<RecipeType[]>, args: string[]) {
+export async function createApp(store: Store<RecipeType[]>, args: string[], ) {
   const [, , command, ...restArgs] = args;
-
+  
   const commands: Record<string, Command> = {
-    list: list,
-  };
+    'list': list,
+    'details': detailsByRecipeId
+  }
 
-  if (command in commands) {
-    const commandFunction = commands[command];
+  if(command in commands) {
+    const commandFunction = commands[command] 
     await commandFunction(store, restArgs);
   } else {
     throw new AppError(`Unknown command: ${command}`);
--- src/commands.ts
+++ src/commands.ts
@@ -5,8 +5,8 @@ export async function list(store: Store<RecipeType[]>, args: string[]) {
   const recipe = new Recipe(store);
   const recipes = await recipe.readAll();
 
-  if(args.length > 0) {
-    throw new Error ("Wrong command: list should not have any arguments")
+  if (args.length > 0) {
+    throw new Error('Wrong command: list should not have any arguments');
   }
 
   const formatted = recipes
@@ -14,4 +14,29 @@ export async function list(store: Store<RecipeType[]>, args: string[]) {
     .join('\n');
   console.log('Your recipes:');
   console.log(formatted);
+}
+
+export async function detailsByRecipeId(
+  store: Store<RecipeType[]>,
+  args: string[]
+) {
+  const recipe = new Recipe(store);
+  const recipes = await recipe.readAll();
+
+  if (args.length > 1 || isNaN(parseInt(args[0]))) {
+    throw new Error('Please provide a numeric value only as an ID');
+  }
+
+  const id = parseInt(args[0]);
+
+  const recipeToFind: RecipeType[] = recipes.filter(
+    (recipe) => recipe.id === id
+  );
+  if (id <= recipes.length) {
+    console.log(
+      `ID: ${recipeToFind[0].id}` + '\n' + `Name: ${recipeToFind[0].name}`
+    );
+  } else {
+    console.log('There is no recipe with that ID');
+  }
 }
